aa9b863f37a33f44134c96b379326ec7dbcf06e0
Kuba <horda1@op.pl>
2017-01-18 20:18:49 +0100

    [+] New project

---
+++ ProjectJenkins/ProjectJenkins/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectJenkins
+{
+    [TestFixture]
+    public class TestClass
+    {
+
+        public void TestMethod2()
+        {
+            Assert.Pass("Second Test");
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
